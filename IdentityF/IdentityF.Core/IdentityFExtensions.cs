dff0071fa89538fd80fe50255db7696af0fa5937
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2024-02-18 17:27:49 +0200

    Fix AddIdentityFServices

--- IdentityF/IdentityF.Core/IdentityFExtensions.cs
+++ IdentityF/IdentityF.Core/IdentityFExtensions.cs
@@ -28,74 +28,7 @@ namespace IdentityF
     {
         public static IServiceCollection AddIdentityFServices(this IServiceCollection services)
         {
-            var identityOptions = new IdentityFOptions();
-
-            SmsDependencies.Register(services);
-            EmailDependencies.Register(services);
-            ManagersDependencies.Register(services);
-            SignUpDependencies.Register(services);
-            AuthDependencies.Register(services);
-            SignInDependencies.Register(services);
-            LocationDependencies.Register(services);
-            SessionsDependencies.Register(services);
-            ConfirmDependencies.Register(services);
-            services.AddScoped<IDatabaseService, IdentityDatabaseService>();
-
-            var sessionManager = identityOptions.Token.SessionManager;
-            if (sessionManager.Implementation != null && typeof(ISessionManager).IsAssignableFrom(sessionManager.Implementation))
-            {
-                if (sessionManager.Lifetime == ServiceLifetime.Transient)
-                    services.AddTransient(typeof(ISessionManager), sessionManager.Implementation);
-                if (sessionManager.Lifetime == ServiceLifetime.Scoped)
-                    services.AddScoped(typeof(ISessionManager), sessionManager.Implementation);
-                if (sessionManager.Lifetime == ServiceLifetime.Singleton)
-                    services.AddSingleton(typeof(ISessionManager), sessionManager.Implementation);
-            }
-            else
-            {
-                services.AddSingleton<ISessionManager, InMemorySessionManager>();
-            }
-
-            services.AddDateTimeProvider();
-            services.AddHttpContextAccessor();
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                        .AddJwtBearer(jwt =>
-                        {
-                            jwt.RequireHttpsMetadata = false;
-                            jwt.SaveToken = true;
-                            jwt.TokenValidationParameters = new TokenValidationParameters
-                            {
-                                ValidateIssuer = true,
-                                ValidIssuer = identityOptions.Token.Issuer,
-                                ValidateAudience = false,
-                                ValidateLifetime = true,
-                                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(identityOptions.Token.SecretKey)),
-                                ValidateIssuerSigningKey = true
-                            };
-                        });
-
-            var db = identityOptions.DbConnection;
-            services.AddDbContext<IdentityFContext>(options =>
-            {
-                if (db.DatabaseProvider == DatabaseProviders.Sqlite)
-                {
-                    options.UseSqlite(db.ConnectionString);
-                }
-                if (db.DatabaseProvider == DatabaseProviders.SqlServer)
-                {
-                    options.UseSqlServer(db.ConnectionString);
-                }
-                if (db.DatabaseProvider == DatabaseProviders.Postgres)
-                {
-                    options.UseNpgsql(db.ConnectionString);
-                    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-                }
-                if (db.DatabaseProvider == DatabaseProviders.MySql)
-                {
-                    options.UseMySql(db.ConnectionString, ServerVersion.AutoDetect(db.ConnectionString));
-                }
-            });
-            return services;
+            return services.AddIdentityFCore(new IdentityFOptions());
         }
 
         public static IServiceCollection AddIdentityFServices(this IServiceCollection services, Action<IdentityFOptions> optionsAction)
@@ -106,7 +39,11 @@ namespace IdentityF
                 services.Configure(optionsAction);
                 optionsAction(identityOptions);
             }
+            return services.AddIdentityFCore(identityOptions);
+        }
 
+        private static IServiceCollection AddIdentityFCore(this IServiceCollection services, IdentityFOptions identityOptions)
+        {
             SmsDependencies.Register(services);
             EmailDependencies.Register(services);
             ManagersDependencies.Register(services);
